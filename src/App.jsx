a4a72660ae76d19ce277fda26bfdb1353eb2ed4b
dennispaul8 <jusmepaul8@gmail.com>
2024-06-03 16:45:46 +0100

    Add project section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AnimatedCursor from 'react-animated-cursor'
 import { Navbar } from './components/navbar/Navbar.jsx'
 import { Hero } from './components/hero/Hero.jsx'
 import { About } from './components/about/About.jsx'
+import { Projects } from './components/projects/Projects.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
      <Navbar/>
      <Hero/>
      <About/>
+     <Projects/>
     </>
   )
 }
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,15 @@
+import './Projects.css'
+
+export const Projects = () => {
+
+    return (
+
+        <>
+            <div className="projects-cont">
+                
+                <h3>Projects coming soon</h3>
+
+            </div>
+        </>
+    )
+}
