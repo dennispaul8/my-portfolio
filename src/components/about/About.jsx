0714424bd3e491edc84b035384519ac01de372eb
dennispaul8 <jusmepaul8@gmail.com>
2024-06-03 16:39:14 +0100

    Complete about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,21 @@
 import './About.css'
 
-export const About =()=>{
+export const About = () => {
 
-    return(
+    return (
 
         <>
             <div className="about-cont">
-               <h3> Hi, I'm Dennis.</h3>
-               <p>I'm a Frontend Developer. My passion is to create and develop a clean UI/UX for my users.</p>
+                <img src="../public/hero-devices.svg" alt="" />
 
-                <p>My main stack currently is React.js in combination with Bootstrap CSS</p>
+                <div className="about-txt">
+                    <h2 className='about-heading'>About Me</h2>
+                    <p className='about-role'>Front-end Developer based in Nigeria </p>
+                    <p className='about-name'>Hey, my name is Dennis, and I'm a Frontend Developer. My passion is to create and develop a clean UI/UX for my users.</p>
+
+                    <p className='about-stack'>My main stack currently is React.js in combination with Bootstrap CSS</p>
+                </div>
+
             </div>
         </>
     )
