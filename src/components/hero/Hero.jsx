cf316043a0ff0564e17ab44a63b6f4bafd6062f9
dennispaul8 <jusmepaul8@gmail.com>
2024-06-05 11:19:40 +0100

    Add tech stack icons and modification

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,14 +7,23 @@ export const Hero = () => {
         <>
             <div className="hero-cont">
                 <div className="hero-txt">
-                  <h1>Front-End React Developer</h1>
-                 <p className='hero-txt--about'>Hi, I'm Dennis. A passionate Front-end React <br /> Developer based in Nigeria. </p>
-                 <p className='tech-stack--txt'>Tech Stack: <span>icons</span></p>
+                    <h1>Front-End React Developer</h1>
+                    <p className='hero-txt--about'>Hi, I'm Dennis. A passionate Front-end React <br /> Developer based in Nigeria. </p>
+                    
+                    
+                    <div className="tech-stack--cont">
+                    <p className='tech-stack--txt'>Tech Stack |</p>
+                    <div className='skill-icons--cont'> 
+                                <img className='html-js--skills' src="https://skillicons.dev/icons?i=html,js" />
+                                <img className='bs-tw--skills' src="https://skillicons.dev/icons?i=bootstrap,tailwind" />
+                                <img className='react--skill' src="https://skillicons.dev/icons?i=react,next" />
+                        </div>
+                        </div>
                 </div>
-             
-             <div className="hero-img-cont">     
-                <img src="../public/avatar.svg" alt="" />
-             </div>
+
+                <div className="hero-img-cont">
+                    <img src="../public/avatar.svg" alt="" />
+                </div>
 
                 {/* <div className='hero-device'>
                     <img src="../public/hero-devices.svg" alt="" />
