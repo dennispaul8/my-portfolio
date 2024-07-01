933e9ed44b5145f29d29426ed64a4227827a459e
dennispaul8 <jusmepaul8@gmail.com>
2024-07-01 09:56:21 +0100

    Complete tech-stack section for desktop view

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -9,16 +9,7 @@ export const Hero = () => {
                 <div className="hero-txt">
                     <h1>Front-End React Developer</h1>
                     <p className='hero-txt--about'>Hi, I'm Dennis. A passionate Front-end React <br /> Developer based in Nigeria. </p>
-                    
-                    
-                    <div className="tech-stack--cont">
-                    <p className='tech-stack--txt'>Tech Stack |</p>
-                        <div className='skill-icons--cont'> 
-                                <img className='html-js--skills' src="https://skillicons.dev/icons?i=html,js" />
-                                <img className='bs-tw--skills' src="https://skillicons.dev/icons?i=bootstrap,tailwind" />
-                                <img className='react--skill' src="https://skillicons.dev/icons?i=react,next" />
-                        </div>
-                        </div>
+                   
                 </div>
 
                 <div className="hero-img-cont">
--- src/components/tech-stack/TechStack.jsx
+++ src/components/tech-stack/TechStack.jsx
@@ -1,5 +1,11 @@
 import './TechStack.css'
-import img from '../../../public/hero-devices.svg'
+import htmlicon from '../../../public/html-icon.svg'
+import cssicon from '../../../public/css-icon.svg'
+import jsicon from '../../../public/js-icon.svg'
+import reacticon from '../../../public/react-icon.svg'
+import bootstrapicon from '../../../public/bootstrap-icon.svg'
+import vscodeicon from '../../../public/vscode-icon.svg'
+import githubicon from '../../../public/github-icon.svg'
 
 export const TechStack = () => {
 
@@ -13,6 +19,15 @@ export const TechStack = () => {
                     <p className='tech-stack-tagline'> Technologies I’ve been working with recently</p>
                 </div>
 
+                <div className='tech-icons'> 
+                                <img className='skill-img' src={htmlicon} />
+                                <img className='skill-img' src={cssicon} />
+                                <img className='skill-img' src={jsicon} />
+                                <img className='skill-img' src={reacticon} />
+                                <img className='skill-img' src={bootstrapicon} />
+                                <img className='skill-img' src={vscodeicon} />
+                                <img className='skill-img' src={githubicon} />
+                        </div>
             </div>
         </>
     )
