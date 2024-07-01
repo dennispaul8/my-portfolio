1ad0afcaaa54ea90ebd55c06be3a04ecf0f7c5a2
dennispaul8 <jusmepaul8@gmail.com>
2024-07-01 16:15:09 +0100

    Edit hero  section for desktop view

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,10 @@
 import './Hero.css'
 import img from "../../../public/avatar.svg";
+import { Link } from 'react-router-dom';
+import githubIcon from '../../../public/github-icon.png'
+import twitterIcon from '../../../public/twitter-icon.png'
+import linkedinIcon from '../../../public/linkedin-icon.png'
+
 export const Hero = () => {
 
     return (
@@ -9,7 +14,17 @@ export const Hero = () => {
                 <div className="hero-txt">
                     <h1>Front-End React Developer</h1>
                     <p className='hero-txt--about'>Hi, I'm Dennis. A passionate Front-end React <br /> Developer based in Nigeria. </p>
-                   
+
+
+                    <div className="social-cont">
+                        <p className='social-txt'>Socials |
+                            <span className="social-cont--icon">
+                                <Link to="https://www.github.com/dennispaul8" target="_blank"><img src={githubIcon} alt="" /></Link>
+                                <Link to="https://www.twitter.com/dennis_icode" target="_blank"><img src={twitterIcon} alt="" /></Link>
+                                <Link to="https://www.linkedin.com/in/oluwajuwonlo-ogunyemi/" target="_blank"><img src={linkedinIcon} alt="" /></Link>
+                            </span>
+                        </p>
+                    </div>
                 </div>
 
                 <div className="hero-img-cont">
