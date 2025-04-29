9e806f0a7ed7685200397711fea9fed6748e8b2b
dennispaul8 <jusmepaul8@gmail.com>
2025-04-29 13:36:37 +0100

    update hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,36 +1,46 @@
-import './Hero.css'
+import "./Hero.css";
 import img from "../../../public/avatar.svg";
-import { Link } from 'react-router-dom';
-import githubIcon from '../../../public/github-icon.png'
-import twitterIcon from '../../../public/twitter-icon.png'
-import linkedinIcon from '../../../public/linkedin-icon.png'
+import { Link } from "react-router-dom";
+import githubIcon from "../../../public/github-icon.png";
+import twitterIcon from "../../../public/twitter-icon.png";
+import linkedinIcon from "../../../public/linkedin-icon.png";
 
 export const Hero = () => {
+  return (
+    <>
+      <div className="hero-cont">
+        <div className="hero-txt">
+          <h1>Front-End React Developer</h1>
+          <p className="hero-txt--about">
+            Hi, I'm Oluwajuwonlo. A Front-end Developer based in Nigeria. <br />{" "}
+            Let’s build something great together.{" "}
+          </p>
 
-    return (
+          <div className="social-cont">
+            <p className="social-txt">
+              Socials |
+              <span className="social-cont--icon">
+                <Link to="https://www.github.com/dennispaul8" target="_blank">
+                  <img src={githubIcon} alt="" />
+                </Link>
+                <Link to="https://www.twitter.com/dennis_icode" target="_blank">
+                  <img src={twitterIcon} alt="" />
+                </Link>
+                <Link
+                  to="https://www.linkedin.com/in/oluwajuwonlo-ogunyemi/"
+                  target="_blank"
+                >
+                  <img src={linkedinIcon} alt="" />
+                </Link>
+              </span>
+            </p>
+          </div>
+        </div>
 
-        <>
-            <div className="hero-cont">
-                <div className="hero-txt">
-                    <h1>Front-End React Developer</h1>
-                    <p className='hero-txt--about'>Hi, I'm Dennis. A passionate Front-end React <br /> Developer based in Nigeria. </p>
-
-
-                    <div className="social-cont">
-                        <p className='social-txt'>Socials |
-                            <span className="social-cont--icon">
-                                <Link to="https://www.github.com/dennispaul8" target="_blank"><img src={githubIcon} alt="" /></Link>
-                                <Link to="https://www.twitter.com/dennis_icode" target="_blank"><img src={twitterIcon} alt="" /></Link>
-                                <Link to="https://www.linkedin.com/in/oluwajuwonlo-ogunyemi/" target="_blank"><img src={linkedinIcon} alt="" /></Link>
-                            </span>
-                        </p>
-                    </div>
-                </div>
-
-                <div className="hero-img-cont">
-                    <img src={img} alt="avatar" />
-                </div>
-            </div>
-        </>
-    )
-}
+        <div className="hero-img-cont">
+          <img src={img} alt="avatar" />
+        </div>
+      </div>
+    </>
+  );
+};
